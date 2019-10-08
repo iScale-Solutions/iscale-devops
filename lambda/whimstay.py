f5f26aa8a045c45115a6c0e17adb8945b1a21f9a
kmelchor <klarenz@iscale-solutions.com>
2019-10-08 17:50:55 +0800

    Add email notification function

--- lambda/whimstay.py
+++ lambda/whimstay.py
@@ -9,6 +9,7 @@ def lambda_handler(event, context):
     start=resources.find("start")
     
     ec2 = boto3.client('ec2',region_name=os.environ['REGION'])
+    client = boto3.client('ses',region_name=os.environ['SESREGION'])
     
     try:
         # Checks if trigger/event is start or shutdown
@@ -18,5 +19,35 @@ def lambda_handler(event, context):
         elif start >0:
             ec2.start_instances(InstanceIds=[os.environ['INSTANCEID']],DryRun=False)
             print('[INFO] Started your instances: ' + str(os.environ['INSTANCEID']))
+            
+            instance = ec2.describe_instances(InstanceIds=[os.environ['INSTANCEID']])
+            public_ip = instance['Reservations'][0]['Instances'][0]['PublicIpAddress']
+            
+            # Email settings
+            subject = "New Proxy IP Generated"
+            body = ("This is an auto-generated message. Do not reply. \r\n"
+                    "You may connect to your whimstay proxy server with the following IP: " 
+                    + public_ip)
+            charset = "utf8"
+    
+            response = client.send_email(
+                                Destination={
+                                    'ToAddresses': [os.environ['RECIPIENT']]
+                                },
+                                Message={
+                                    'Body': {
+                                        'Text': {
+                                            'Charset': charset,
+                                            'Data': body,
+                                        },
+                                    },
+                                    'Subject': {
+                                        'Charset': charset,
+                                        'Data': subject,
+                                    },
+                                },
+                                Source=os.environ['SENDER']
+                            )
+            
     except Exception as e:
         raise e
