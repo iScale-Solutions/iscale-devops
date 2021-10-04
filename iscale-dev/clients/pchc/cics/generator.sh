aa088dd5e6e48233ead93ac64d20994ee34cd960
Iannis Hanen <iannis@iscale-solutions.com>
2021-10-04 13:38:25 +0800

    Using Yaml template file with generator

--- iscale-dev/clients/pchc/cics/generator.sh
+++ iscale-dev/clients/pchc/cics/generator.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-echo "" > output.yaml
+echo "" > sqs.yaml
 while read p; do
-    sed "s,XXX,${p}," bank_template.yaml >> output.yaml
+    sed "s,XXX,${p}," bank_template.yaml >> sqs.yaml
 done <bank_list.txt
+sed -e '/#XXX_INSERT_SQS_XXX/r./sqs.yaml' cics_template.yaml > cics.yaml
+rm -f sqs.yaml
