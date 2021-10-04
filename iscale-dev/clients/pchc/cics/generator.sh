38d9ae64ea5745f9cbbaa9053c8b94cbfcef3b7e
Iannis Hanen <iannis@iscale-solutions.com>
2021-10-04 14:29:07 +0800

    Generating notifications from S3 bucket

--- iscale-dev/clients/pchc/cics/generator.sh
+++ iscale-dev/clients/pchc/cics/generator.sh
@@ -2,6 +2,13 @@
 echo "" > sqs.yaml
 while read p; do
     sed "s,XXX,${p}," bank_template.yaml >> sqs.yaml
+    sed "s,XXX,${p}," notification_template.yaml >> notification.yaml
 done <bank_list.txt
-sed -e '/#XXX_INSERT_SQS_XXX/r./sqs.yaml' cics_template.yaml > cics.yaml
+sed "s,#NotificationConfiguration:,      NotificationConfiguration:," cics_template.yaml > cics_template2.yaml
+sed "s,#QueueConfigurations,        QueueConfigurations," cics_template2.yaml > cics_template.yaml
+sed -e '/#XXX_INSERT_SQS_XXX/r./sqs.yaml' cics_template.yaml > cics_1.yaml
+sed -e '/#XXX_NOTIFICATION_CONFIG/r./notification.yaml' cics_1.yaml > cics.yaml
+rm -r cics_template2.yaml
+rm -r cics_1.yaml
 rm -f sqs.yaml
+rm -f notification.yaml
