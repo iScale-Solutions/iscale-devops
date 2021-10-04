dbaf132b1582d582a749b31400b27a20fd024c19
Iannis Hanen <iannis@iscale-solutions.com>
2021-10-04 14:33:32 +0800

    Minor change for NotificationConfiguration codegen

--- iscale-dev/clients/pchc/cics/generator.sh
+++ iscale-dev/clients/pchc/cics/generator.sh
@@ -5,10 +5,11 @@ while read p; do
     sed "s,XXX,${p}," notification_template.yaml >> notification.yaml
 done <bank_list.txt
 sed "s,#NotificationConfiguration:,      NotificationConfiguration:," cics_template.yaml > cics_template2.yaml
-sed "s,#QueueConfigurations,        QueueConfigurations," cics_template2.yaml > cics_template.yaml
-sed -e '/#XXX_INSERT_SQS_XXX/r./sqs.yaml' cics_template.yaml > cics_1.yaml
+sed "s,#QueueConfigurations:,        QueueConfigurations:," cics_template2.yaml > cics_template3.yaml
+sed -e '/#XXX_INSERT_SQS_XXX/r./sqs.yaml' cics_template3.yaml > cics_1.yaml
 sed -e '/#XXX_NOTIFICATION_CONFIG/r./notification.yaml' cics_1.yaml > cics.yaml
-rm -r cics_template2.yaml
-rm -r cics_1.yaml
+rm -f cics_template2.yaml
+rm -f cics_template3.yaml
+rm -f cics_1.yaml
 rm -f sqs.yaml
 rm -f notification.yaml
